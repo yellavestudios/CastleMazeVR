e4b1e9d0536b786c457eb2e630a53e247f7285ad
Christopher Pearl <53843747+yellavestudios@users.noreply.github.com>
2022-01-03 14:42:54 -0800

    Add files via upload

---
+++ GazeInputModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInputModule : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
